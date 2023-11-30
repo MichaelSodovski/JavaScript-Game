e47f53d1314a575a872955e0552dad62f8d6d34e
MichaelSodovski <michaelsod90@gmail.com>
2023-11-30 10:37:23 +0200

    foundations

---
+++ main.js
@@ -0,0 +1,13 @@
+function createPuzzlePieces(rows, cols) {
+    let gameBoardContainer = document.querySelector(".game-board-container")
+    
+
+
+
+    gameBoardContainer.appendChild();
+}
+
+
+function closeWindow() {
+    window.close();
+}
